5f2c6d2ce28d3d4472c5a29517aec8017e592b6b
random-entity <73927400+random-entity@users.noreply.github.com>
2021-10-09 23:30:44 +0900

    매치 검색 후 인력 작용

--- Assets/Rock.cs
+++ Assets/Rock.cs
@@ -2,29 +2,49 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+[RequireComponent(typeof(Rigidbody))]
 public class Rock : MonoBehaviour
 {
+    #region fields
     private string username;
-    private List<string> wishlist;
-    private List<string> mercylist;
+    private List<string> wishlist, mercylist;
     private Text wishesText, merciesText, usernameText;
+    private List<Rock> notYetChecked;
+    private List<Rock> matches;
+    public Rigidbody rb;
+    #endregion
 
-    public void setUsername(string username) {
+    #region getters and setters
+    public void setUsername(string username)
+    {
         this.username = username;
         usernameText.text = this.username;
     }
-
-    private void Awake()
+    public List<string> getMercylist()
     {
-        wishlist = new List<string>();
-        mercylist = new List<string>();
-
-        Text[] texts = GetComponentsInChildren<Text>();
-        wishesText = texts[0];
-        merciesText = texts[1];
-        usernameText = texts[2];
+        return mercylist;
     }
+    private string getWishesString()
+    {
+        string wishes = "";
+        foreach (string wish in wishlist)
+        {
+            wishes += wish + " ";
+        }
+        return wishes;
+    }
+    private string getMerciesString()
+    {
+        string mercies = "";
+        foreach (string mercy in mercylist)
+        {
+            mercies += mercy + " ";
+        }
+        return mercies;
+    }
+    #endregion
 
+    #region methods for initializing data
     private void addData(string input, List<string> list)
     {
         string[] splitInput = input.Split(' ');
@@ -41,31 +61,87 @@ public class Rock : MonoBehaviour
     {
         addData(input, mercylist);
     }
-
-    private void searchMatch() {
-        foreach(string wish in wishlist ) {
-            
+    #endregion
+   
+    #region AddRock event handling
+    private void OnEnable()
+    {
+        RockManager.AddRockAlert += OnAddRock;
+    }
+    private void OnDisable()
+    {
+        RockManager.AddRockAlert -= OnAddRock;
+    }
+    private void OnAddRock(Rock newRock)
+    {
+        if (newRock != this)
+        {
+            notYetChecked.Add(newRock);
+            searchMatch();
         }
     }
+    #endregion
 
-    private string getWishesString() {
-        string wishes = "";
-        foreach(string wish in wishlist) {
-            wishes += wish + " ";
-        }
-        return wishes;
+    private void Awake()
+    {
+        wishlist = new List<string>();
+        mercylist = new List<string>();
+        notYetChecked = new List<Rock>();
+        matches = new List<Rock>();
+
+        Text[] texts = GetComponentsInChildren<Text>();
+        wishesText = texts[0];
+        merciesText = texts[1];
+        usernameText = texts[2];
+
+        rb = GetComponent<Rigidbody>();
     }
-    private string getMerciesString() {
-        string mercies = "";
-        foreach(string mercy in mercylist) {
-            mercies += mercy + " ";
+    private void Start()
+    {
+        wishesText.text = getWishesString();
+        merciesText.text = getMerciesString();
+    }
+
+    private void searchMatch()
+    {
+        foreach (string myWish in wishlist)
+        {
+            foreach (Rock other in notYetChecked)
+            {
+                foreach (string othersmercy in other.getMercylist())
+                {
+                    if (othersmercy == myWish)
+                    {
+                        matches.Add(other);
+                    }
+                }
+            }
         }
-        return mercies;
+        notYetChecked.Clear();
     }
 
     void Update()
     {
-        wishesText.text = getWishesString();
-        merciesText.text = getMerciesString();
+        addForce();
+    }
+
+    private void addForce() {
+        foreach(Rock other in matches) {
+            Vector3 dir = other.transform.position - transform.position;
+            dir = Vector3.Normalize(dir);
+
+            rb.AddForce(dir, ForceMode.Force);
+            other.rb.AddForce(-dir, ForceMode.Force);
+        }
+    }
+    // private static Vector3 getForce(Vector3 other) {
+
+    // }
+
+
+    private void OnDrawGizmos() {
+        foreach(Rock other in matches) {
+            Gizmos.DrawLine(transform.position, other.transform.position);
+        }
     }
 }
--- Assets/RockManager.cs
+++ Assets/RockManager.cs
@@ -1,18 +1,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public delegate void RockEvent(Rock newRock);
+
 public class RockManager : MonoBehaviour
 {
+    // private static string emojiPresetsString = "😃 🤢 🤮 🤕 👿 👹 🤡 💩 👻 💀 👽 👾 🤖 🎃 😺 🍗 🍤 🍺 🍿 🧘 🎸 ✈️ 🏝";
+    // private static string[] emojiPresets;
+    private static string[] wishPresets = { "강아지", "고양이", "맛있는거", "친구", "춤", "취직", "운동", "성적", "아이폰", "애인", "건강", "가족", "천재", "돈", "시력" };
+
+    private static int temp;
+
+    public static RockEvent AddRockAlert;
     public static List<Rock> rockList;
     [SerializeField] private Rock rockPrefab;
 
     private void Awake()
     {
         rockList = new List<Rock>();
-
-        addRock("chu", "dog cat cola", "friend dance job");
-        addRock("ku", "cat soccer dance", "cola dog boyfriend");
-        addRock("mi", "grade health muscle iphone", "friend girlfriend house");
     }
 
     public void addRock(string username, string wishes, string mercies)
@@ -24,5 +29,16 @@ public class RockManager : MonoBehaviour
         newRock.addMercyData(mercies);
 
         rockList.Add(newRock);
+
+        AddRockAlert(newRock);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            temp++;
+            addRock("USER" + temp, wishPresets[Random.Range(0, wishPresets.Length)], wishPresets[Random.Range(0, wishPresets.Length)]);
+        }
     }
 }
