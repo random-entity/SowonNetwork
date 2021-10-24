24a4309276d0a3fbed98280f70ad9cee235091be
random-entity <73927400+random-entity@users.noreply.github.com>
2021-10-25 04:46:13 +0900

    Revert "Revert "이모지 개수 줄이기 전 커밋""
    This reverts commit 57c60ab6030a9259c9f7bf9a16342333312dbf81.

--- Assets/Scripts/EnvironmentSpecs.cs
+++ Assets/Scripts/EnvironmentSpecs.cs
@@ -1,7 +1,7 @@
 public struct EnvironmentSpecs
 {
-    public static float boundXLeft = -10f, boundXRight = 10f;
-    public static float boundYBottom = 10f, boundYTop = 30f;
+    public static float boundXLeft = -25f, boundXRight = 25f;
+    public static float boundYBottom = 10f, boundYTop = 45f;
     public static float boundYBottomSinked = 0f;
     public static float boundZFront = 0f, boundZBack = 2f;
 }
