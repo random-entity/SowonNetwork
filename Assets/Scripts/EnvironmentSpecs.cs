e2ff11aff3db44f0df53a694e315977528724ca2
random-entity <73927400+random-entity@users.noreply.github.com>
2021-10-26 13:08:33 +0900

    Update EnvironmentSpecs.cs

--- Assets/Scripts/EnvironmentSpecs.cs
+++ Assets/Scripts/EnvironmentSpecs.cs
@@ -1,7 +1,7 @@
 public struct EnvironmentSpecs
 {
-    public static float boundXLeft = -80f, boundXRight = 80f;
-    public static float boundYBottom = 15f, boundYTop = 70f;
+    public static float boundXLeft = -50f, boundXRight = 50f;
+    public static float boundYBottom = 15f, boundYTop = 60f;
     public static float boundYBottomSinked = 0f;
     public static float boundZFront = 0f, boundZBack = 2f;
 }
